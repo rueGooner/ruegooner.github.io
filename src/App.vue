e63f67b45dab69b7f870627f4e87b10d01535a66
rue <rue.cumberbatch@bt.com>
2024-10-29 23:31:00 +0000

    feat: port resumeIntro component over

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div class="container px-3 px-lg-5">
     <div class="resume-wrapper mx-auto theme-bg-light p-5 mb-5 my-5 shadow-lg">
       <ResumeHeader />
+      <hr/>
+      <ResumeIntro />
     </div>
   </div>
 </template>
 
 <script>
 import ResumeHeader from "@/components/ResumeHeader.vue";
+import ResumeIntro from "@/components/ResumeIntro.vue";
 
 export default {
   name: 'App',
   components: {
+    ResumeIntro,
     ResumeHeader,
   }
 }
---
+++ src/components/ResumeIntro.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="resume-intro py-3">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-3 col-xl-2 text-center">
+        <img
+          class="resume-profile-image mb-3 mb-md-0 me-md-5 ms-md-0 rounded mx-auto"
+          src="../assets/me.png"
+          alt="image"
+        />
+      </div>
+
+
+      <div class="col text-start">
+        <p class="mb-0">
+          Experienced Web Developer with over 7 years’ experience across
+          all areas of the Web Application development life cycle
+          currently acting as the Lead on a number of key internal
+          projects. An efficient team player with a can-do approach to
+          delivering software solutions and an excellent capacity for
+          handling new technologies, picking up new skills and solving
+          problems. Proficiency in JavaScript delivering solutions using
+          both the MEVN and MERN tech stacks, ticketing with JIRA,
+          version control with Bitbucket, unit testing with Jest,
+          React/Vue Test Utils and TypeScript.
+        </p>
+      </div>
+      <!--//col-->
+    </div>
+  </div>
+</template>
