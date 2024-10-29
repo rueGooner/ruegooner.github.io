fee2e778df75f4956e539335fb2b36dd7e9ced43
rue <rue.cumberbatch@bt.com>
2024-10-29 23:19:33 +0000

    feat: move header content into dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container px-3 px-lg-5">
+    <div class="resume-wrapper mx-auto theme-bg-light p-5 mb-5 my-5 shadow-lg">
+      <ResumeHeader />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ResumeHeader from "@/components/ResumeHeader.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ResumeHeader,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ResumeHeader.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Icon } from '@iconify/vue';
+
+const headerData = ref(null);
+
+onMounted(async () => {
+  const response = await import('@/assets/header.json');
+  headerData.value = response.default;
+});
+</script>
+
+<template>
+  <div class="resume-header" v-if="headerData">
+    <div class="row align-items-center">
+      <div class="resume-title col-12 col-md-6 col-lg-8 col-xl-9">
+        <h2 class="resume-name mb-0 text-uppercase">
+          {{ headerData.name }}
+        </h2>
+        <div class="resume-tagline mb-3 mb-md-0">
+          {{  headerData.tagline }}
+        </div>
+      </div>
+      <!--//resume-title-->
+      <div class="resume-contact col-12 col-md-6 col-lg-4 col-xl-3">
+        <ul class="list-unstyled mb-0">
+          <li class="mb-2" v-for="(contact, index) in headerData.contact" :key="index">
+            <Icon :icon="contact.icon" class="h6 mb-1 me-2" />
+            {{ contact.text }}
+          </li>
+        </ul>
+      </div>
+      <!--//resume-contact-->
+    </div>
+    <!--//row-->
+  </div>
+</template>
+
+<style scoped>
+
+</style>
